a5011cee2b45bfe86c4130065792e6a115b2f0a7
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-08-16 12:36:50 +0530

    commit

--- example-base-11/utility-lib/src/playground/util/custom-logger.js
+++ example-base-11/utility-lib/src/playground/util/custom-logger.js
@@ -1,19 +1,19 @@
-const logMessage = (phase, message = { title: "", data: `No data provided with this ${phase}` }) => {
-  console.log(`${phase} Message: '${message.title}', data: ${JSON.stringify(message.data)}\n =======`);
+const logMessage = (phase, message = { title: "", data: `No data provided with this ${phase}` }, beutify = false) => {
+  console.log(`${phase} Message: '${message.title}', data: ${JSON.stringify(message.data, null, beutify ? 2 : 0)}\n =======`);
 };
 
-const startOperation = (message) => {
+const startOperation = (message, beutify = false) => {
   console.log("\n============ Starting calculations ===================");
-  console.log(new Date().toString()+"\n")
-  logMessage("Start", message);
+  console.log(new Date().toString() + "\n")
+  logMessage("Start", message, beutify);
 };
 
-const stepOperation = (message) => {
-  logMessage("Step", message);
+const stepOperation = (message, beutify = false) => {
+  logMessage("Step", message, beutify);
 };
 
-const endOperation = (message) => {
-  logMessage("End", message);
+const endOperation = (message, beutify = false) => {
+  logMessage("End", message, beutify);
   console.log("\n############# Completing calculations #####################\n");
 };
 
