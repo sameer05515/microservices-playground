fd687d1f7560de6e2ef1f490b46effa5b75244fb
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-08-16 20:12:33 +0530

    commit

--- example-base-11/utility-lib/src/playground/text-indentation/test/buildtree.test.with-custom-logger.js
+++ example-base-11/utility-lib/src/playground/text-indentation/test/buildtree.test.with-custom-logger.js
@@ -8,32 +8,29 @@ const buildTree = require('../buildTree');
 //     //console.log(`\n=========== Index of SAMPLE: '${index}' ====================`, '\n');
 //     const result = buildTree(textInput.input);
 
-    
-//     console.log(`test no: ${index}, Expected: ${ex1[index].expectedResult}, Actual: ${result.isValid}, Message: ${result.message}`);
-//     // if(result?.data && result.data.length>0){
+//     console.log(`Test No: ${index}, Expected: ${ex1[index].expectedResult}, Actual: ${result.isValid}, Message: ${result.message}`);
+//     // if(result?.data && result.data.length > 0){
 //     //     console.log("++++++++++++++++++++++\n", result.data[0], "\n++++++++++++++++++++++\n")
 //     // }
 //     console.log('============================================');
 //     console.log(`Input: '${ex1[index].input}'`);
 //     console.log('============================================');
-//     console.log(`Result: \n`,getHierarchyString(result.data))
+//     console.log(`Result: \n`, getHierarchyString(result.data));
 
 // })
 
-ex1.map((textInput, index)=>{
-    console.log(`\n=========== Starting proceessing for SAMPLE Index : '${index}' ====================`, '\n');
+ex1.map((textInput, index) => {
+    console.log(`\n=========== Starting Processing for SAMPLE Index: '${index}' ====================`, '\n');
     const result = buildTree(textInput.input);
 
-    
-    console.log(`test no: ${index}, Expected: ${ex1[index].expectedResult}, Actual: ${result.isValid}, Message: ${result.message}`);
-    if(result?.data && result.data.length>0){
-        console.log("++++++++++++++++++++++\n", result.data[0], "\n++++++++++++++++++++++\n")
+    console.log(`Test No: ${index}, Expected: ${ex1[index].expectedResult}, Actual: ${result.isValid}, Message: ${result.message}`);
+    if (result?.data && result.data.length > 0) {
+        console.log("++++++++++++++++++++++\n", result.data[0], "\n++++++++++++++++++++++\n");
     }
     console.log('============================================');
     console.log(`Input: '${ex1[index].input}'`);
     console.log('============================================');
-    console.log(`Result: \n`,getHierarchyString(result.data));
+    console.log(`Result: \n`, getHierarchyString(result.data));
 
-    console.log(`=================== Index of SAMPLE: '${index}' Processed with result: Expected: ${ex1[index].expectedResult}, Actual: ${result.isValid} =========================\n\n`);
-
-})
+    console.log(`=================== Index of SAMPLE: '${index}' Processed with Result: Expected: ${ex1[index].expectedResult}, Actual: ${result.isValid} =========================\n\n`);
+});
