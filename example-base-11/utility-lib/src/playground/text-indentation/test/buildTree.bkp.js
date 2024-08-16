47b5455abd8b6c82c95823a8db1f85a27f5a4730
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-08-16 13:27:37 +0530

    commit

--- example-base-11/utility-lib/src/playground/text-indentation/test/buildTree.bkp.js
+++ example-base-11/utility-lib/src/playground/text-indentation/test/buildTree.bkp.js
@@ -8,18 +8,18 @@ const parseLines = require("./parse-lines");
 
 function buildTree(text) {
   startOperation({
-    title: "Method: buildTree: Start with data: ",
+    title: "Inside Method: [buildTree] buildTree: Start with data: ",
     data: text,
   }, true);
   const parsed = parseLines(text);
   stepOperation({
-    title: "Result post method: parseLines: ",
+    title: "Inside Method: [buildTree]: Result post method: parseLines: ",
     data: { parsed },
   });
 
   if (!parsed.isValid) {
     stepOperation({
-      title: "Result for checking 'parsed.isValid' is 'falsy' : ",
+      title: "Inside Method: [buildTree]: Result for checking 'parsed.isValid' is 'falsy' : ",
       data: {
         data: parsed.data,
         isValid: false,
@@ -36,17 +36,17 @@ function buildTree(text) {
   }
 
   stepOperation({
-    title: "Result for checking 'parsed.isValid' is 'truthy' : Will start validate method with parsedData : ",
+    title: "Inside Method: [buildTree]: Result for checking 'parsed.isValid' is 'truthy' : Will start validate method with parsedData : ",
     data: { parsedData:parsed.data },
   });
 
   const dataAfterValidation= validate(parsed.data);
   stepOperation({
-    title: "Data recieved from 'validate' method ",
+    title: "Inside Method: [buildTree]: Data recieved from 'validate' method ",
     data: { dataAfterValidation },
   });
 
-  endOperation({ title: "Method End with data: ", data: dataAfterValidation });
+  endOperation({ title: "Inside Method: [buildTree]: Method End with data: ", data: dataAfterValidation });
   return dataAfterValidation;
 }
 
@@ -59,7 +59,7 @@ const textInput = `
     Node 2
 `;
 
-const result = buildTree(textInput);
+// const result = buildTree(textInput);
 // console.log(result);
 
 module.exports = buildTree;
