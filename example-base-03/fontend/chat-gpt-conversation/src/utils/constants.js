69d2ab8a1c2ef299a50b9bb52c37d012eeffaefe
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-10-21 16:29:06 +0530

    commit

--- example-base-03/fontend/chat-gpt-conversation/src/utils/constants.js
+++ example-base-03/fontend/chat-gpt-conversation/src/utils/constants.js
@@ -16,9 +16,9 @@ export const coversationNames = {
     CONVERSATIONS_18_Aug_2024: "/data/conversations-18-Aug-2024.json",
     CONVERSATIONS_19_Aug_2024: "/data/conversations-19-Aug-2024.json",
     CONVERSATIONS_25_Aug_2024: "/data/conversations-25-Aug-2024.json",
-	CONVERSATIONS_30_Aug_2024: "/data/conversations-30-Aug-2024.json",
-	CONVERSATIONS_06_SEP_2024:"/data/conversations-06-Sep-2024.json",
-    CONVERSATIONS_24_SEP_2024:"/data/conversations-24-Sep-2024.json"
+    CONVERSATIONS_30_Aug_2024: "/data/conversations-30-Aug-2024.json",
+    CONVERSATIONS_06_SEP_2024: "/data/conversations-06-Sep-2024.json",
+    CONVERSATIONS_24_SEP_2024: "/data/conversations-24-Sep-2024.json"
 };
 
 export const LATEST_CONVERSATION_FILE =
