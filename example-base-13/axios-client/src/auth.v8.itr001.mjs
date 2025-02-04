66e58faac8e1f67b4c47ceefe10905a706d6e7bb
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-05 01:23:53 +0530

    commit

--- example-base-13/axios-client/src/auth.v8.itr001.mjs
+++ example-base-13/axios-client/src/auth.v8.itr001.mjs
@@ -2,9 +2,9 @@ import { signup, login } from "./auth-v8/authService.mjs";
 
 const testAuth = async () => {
   try {
-    console.log("🔹 Signing up user...");
-    const signupResponse = await signup("Premendra Kumar","testuser@example.com", "Test@123");
-    console.log("✅ Signup Successful:", signupResponse);
+    // console.log("🔹 Signing up user...");
+    // const signupResponse = await signup("Premendra Kumar","testuser@example.com", "Test@123");
+    // console.log("✅ Signup Successful:", signupResponse);
 
     console.log("\n🔹 Logging in user...");
     const loginResponse = await login("testuser@example.com", "Test@123");
