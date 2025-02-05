c65086f42e7f8df83d827ef76992a910ec94e118
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-05 11:25:50 +0530

    commit

--- example-base-13/axios-client/src/auth.v8.itr002.mjs
+++ example-base-13/axios-client/src/auth.v8.itr002.mjs
@@ -1,22 +1,25 @@
 import { signup, login, getProtectedData, logout } from "./auth-v8/apiClient.itr002.mjs";
 
 const signUpRequired = false;
-const loginRequired=false;
+const loginRequired = false;
+const shouldLogout = false;
 
 // Step 1: Sign up a user, One time activity, hence execution of this route is masked
 if (signUpRequired) {
   await signup("testuser@example.com", "Test@123");
 }
 
-// Step 2: Log in and store token in a file
-if(loginRequired){
+// Step 2: Log in and store token in a file, also this is conditional one.
+if (loginRequired) {
   await login("testuser@example.com", "Test@123");
 }
 
 // Step 3: Access protected API
-await getProtectedData();
+await getProtectedData("/hello");
 
-await getProtectedData("/hello2")
+await getProtectedData("/hello2");
 
 // Step 4: Logout and clear the token
-// logout();
+if (shouldLogout) {
+  logout();
+}
