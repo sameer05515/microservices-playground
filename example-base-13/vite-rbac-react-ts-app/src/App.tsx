f267ee412d61721d2ff8bdb1a5f2cf13c6a744df
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-06 13:57:39 +0530

    commit

--- example-base-13/vite-rbac-react-ts-app/src/App.tsx
+++ example-base-13/vite-rbac-react-ts-app/src/App.tsx
@@ -1,25 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 
-
-import './App.css';
-import Home from './pages/Home';
-import About from './pages/AboutThisProject';
-import NotFound from './pages/NotFound';
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/AboutThisProject";
+import NotFound from "./pages/NotFound";
 // import Layout from "./routes/Layout";
+import TestingDashboard from "./pages/ApnaPlayground";
 
 function App() {
-  
-
   return (
     <div>
-      
       <Routes>
+        {/**Test route- for arbitrary testing from scratch*/}
+        <Route path="/testing" element={<TestingDashboard />} />
+
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- example-base-13/vite-rbac-react-ts-app/src/pages/Home.tsx
+++ example-base-13/vite-rbac-react-ts-app/src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
-    return <h1><nav>
-    <Link to="/">Home</Link> | <Link to="/about">About</Link>
-  </nav></h1>;
-  }
-  
+  return (
+    <h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About </Link> | <Link to="/testing">ApnaPlayground </Link>
+      </nav>
+    </h1>
+  );
+}
