c1184621068968a1c2e21d04d07778734e59b0e0
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-06 20:44:08 +0530

    commit

--- example-base-13/vite-rbac-react-ts-app/src/routes/Layout/Sidebar/v1.tsx
+++ example-base-13/vite-rbac-react-ts-app/src/routes/Layout/Sidebar/v1.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
-import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+// import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+import { AvailableLinks, LinkProps } from "../../available-links";
+import { getIcon } from "../../../common/utils/IconCollection";
 
 export default function Sidebar() {
   return (
@@ -7,7 +9,7 @@ export default function Sidebar() {
       <h2 className="text-2xl font-bold mb-5">Dashboard</h2>
       <nav>
         <ul className="space-y-2">
-          <li>
+          {/* <li>
             <Link to="/" className="flex items-center gap-2 p-2 hover:bg-blue-700 rounded">
               <FiHome /> Home
             </Link>
@@ -21,9 +23,29 @@ export default function Sidebar() {
             <Link to="/settings" className="flex items-center gap-2 p-2 hover:bg-blue-700 rounded">
               <FiSettings /> Settings
             </Link>
-          </li>
+          </li> */}
+
+          {AvailableLinks.map((link) => (
+            <ModuleLink
+              key={link.id}
+              link={link}
+              //   onLinkClick={handleLinkClick}
+            />
+          ))}
         </ul>
       </nav>
     </aside>
   );
 }
+
+const ModuleLink = ({ link }: { link: LinkProps }) => {
+  const { linkPath, linkHeader, icon } = link;
+  const IconComponent = getIcon(icon);
+  return (
+    <li>
+      <Link to={linkPath()} className="flex items-center gap-2 p-2 hover:bg-blue-700 rounded">
+        <IconComponent /> {linkHeader}
+      </Link>
+    </li>
+  );
+};
