20b2ce057fc4ca0b0b104b91e7171a75d20173cc
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-07 19:54:38 +0530

    commit

--- example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/some-old-project-keys.ts
+++ example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/some-old-project-keys.ts
@@ -1,3 +1,11 @@
+/**
+ * **Note:** These keys are just imported from chat-renderer project and not being used in this project.
+ *
+ * However we are retaining them for future reference.
+ * They can guide use some possible key names required in local-storage.
+ *
+ */
+
 export const APPLICATION_OBJECT_CHAT_RENDERER_KEY = "chatRendererConfig";
 export const APPLICATION_OBJECT_THEME_KEY = "viewMode";
 
