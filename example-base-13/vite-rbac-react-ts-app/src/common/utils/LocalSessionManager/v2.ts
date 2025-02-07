85056c511d4f5d34e0d67f8f970435d7cc4dabd3
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-07 19:11:35 +0530

    commit

--- example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/v2.ts
+++ example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/v2.ts
@@ -1,24 +1,17 @@
-/**
- * Local Session Manager
- *
- * Utility to manage application-specific data in local storage.
- * Plan: Import methods from UtilityMethods.ts for validation and other utilities.
- */
-
 import { isValidObject, isValidString } from "../basic-validations";
 
 // Application object keys
 export const APPLICATION_OBJECT_CHAT_RENDERER_KEY = "chatRendererConfig";
-export const APPLICATION_OBJECT_THEME_KEY = "viewMode";
+// export const APPLICATION_OBJECT_THEME_KEY = "viewMode";
 
 // Defined keys used in local storage
-export const KEYS = {
-  listVisible: "listVisible",
-  selectedFileName: "selectedFileName",
-  selectedConversationId: "selectedConversationId",
-  viewMode: "viewMode",
-  ConsolidatedReportV1_SidebarClosed: "ConsolidatedReportV1_SidebarClosed",
-};
+// export const KEYS = {
+//   listVisible: "listVisible",
+//   selectedFileName: "selectedFileName",
+//   selectedConversationId: "selectedConversationId",
+//   viewMode: "viewMode",
+//   ConsolidatedReportV1_SidebarClosed: "ConsolidatedReportV1_SidebarClosed",
+// };
 
 export class LocalSessionManager {
   /**
--- example-base-13/vite-rbac-react-ts-app/src/pages/APIV12Comps/hooks/useAuth.ts
+++ example-base-13/vite-rbac-react-ts-app/src/pages/APIV12Comps/hooks/useAuth.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { LocalSessionManager } from "../../../common/utils/LocalSessionManager/v1";
+// import { LocalSessionManager } from "../../../common/utils/LocalSessionManager/v1";
+import { LocalSessionManager } from "../../../common/utils/LocalSessionManager/v2";
 
 interface User {
   id: string;
