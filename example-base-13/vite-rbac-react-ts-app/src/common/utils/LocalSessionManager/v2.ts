86a43566f5438d440bb0d87a5141f93d01d5d9e7
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-07 19:12:12 +0530

    commit

--- example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/v2.ts
+++ example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/v2.ts
@@ -2,16 +2,7 @@ import { isValidObject, isValidString } from "../basic-validations";
 
 // Application object keys
 export const APPLICATION_OBJECT_CHAT_RENDERER_KEY = "chatRendererConfig";
-// export const APPLICATION_OBJECT_THEME_KEY = "viewMode";
 
-// Defined keys used in local storage
-// export const KEYS = {
-//   listVisible: "listVisible",
-//   selectedFileName: "selectedFileName",
-//   selectedConversationId: "selectedConversationId",
-//   viewMode: "viewMode",
-//   ConsolidatedReportV1_SidebarClosed: "ConsolidatedReportV1_SidebarClosed",
-// };
 
 export class LocalSessionManager {
   /**
