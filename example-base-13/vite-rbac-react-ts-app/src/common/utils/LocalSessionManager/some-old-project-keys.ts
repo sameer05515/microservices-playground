348663d1e6740b763bf24e47e450fe4de2427ec5
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-07 19:10:02 +0530

    commit

---
+++ example-base-13/vite-rbac-react-ts-app/src/common/utils/LocalSessionManager/some-old-project-keys.ts
@@ -0,0 +1,11 @@
+export const APPLICATION_OBJECT_CHAT_RENDERER_KEY = "chatRendererConfig";
+export const APPLICATION_OBJECT_THEME_KEY = "viewMode";
+
+// Defined keys used in local storage
+export const KEYS = {
+  listVisible: "listVisible",
+  selectedFileName: "selectedFileName",
+  selectedConversationId: "selectedConversationId",
+  viewMode: "viewMode",
+  ConsolidatedReportV1_SidebarClosed: "ConsolidatedReportV1_SidebarClosed",
+};
