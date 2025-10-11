cc8adf2b4722be6a982b9446f37e1f231e3932c9
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-10-11 20:08:52 +0530

    commit

--- example-base-03/fontend/chat-gpt-conversation/src/TestingPage/utils.jsx
+++ example-base-03/fontend/chat-gpt-conversation/src/TestingPage/utils.jsx
@@ -49,6 +49,7 @@ import KnowYourPositivityDashboardV1,{ ContentNavigator } from "./KnowYourPositi
 import MDSectionV8TestingV3 from "./MDSectionTesting/v3";
 import ChatDataXRayV1 from "./ChatDataXRay/v1";
 import TimeZoneClockV1 from "./TimeZoneClock/v1";
+import FrenchLearningTrackerV1 from "./FrenchLearningTracker/v1";
 
 // yaha seggregation logic lagana hai ki, asaani se samajh aaye ki kon sa version stable wala hai,
 // abhi yaha bhid bhad dikh rahi hai.
@@ -112,7 +113,8 @@ const Components = {
   KnowYourPositivityDashboardV1,
   MDSectionV8TestingV3,
   ChatDataXRayV1,
-  TimeZoneClockV1
+  TimeZoneClockV1,
+  FrenchLearningTrackerV1
 };
 
 export const componentNames = Object.keys(Components);
